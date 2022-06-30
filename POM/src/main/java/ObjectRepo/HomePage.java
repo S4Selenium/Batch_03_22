be5a922c6a51c08b1b6422e8aca7ac06b063387b
S4Selenium <47279496+Nithinps021@users.noreply.github.com>
2022-06-30 20:27:53 +0530

    POMwithFactory

--- POM/src/main/java/ObjectRepo/HomePage.java
+++ POM/src/main/java/ObjectRepo/HomePage.java
@@ -1,6 +1,26 @@
 package ObjectRepo;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HomePage {
 	//div[@class='app_logo']
-
+	WebDriver driver;
+	
+	
+	public HomePage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+		this.driver=driver;
+		PageFactory.initElements(driver, this);
+	}
+  
+	@FindBy(xpath="abc")
+	WebElement Home;
+	public WebElement username()
+	{
+		return Home;
+	}
 }
